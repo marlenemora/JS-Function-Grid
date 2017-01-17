3fa4bd3fa2840a6b04a76eb71c9ea49e0ae6ab12
Marlene Mora <marlenejmora1@gmail.com>
2017-01-17 23:32:12 +0000

    exercise

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //Write your function calls below this line
+
+    setColor(2, 6, "purple");
+    setColor(1, 5, "purple");
+    setColor(1, 4, "purple");
+    setColor(1, 3, "purple");
+    setColor(2, 2, "purple");
+    setColor(3, 2, "purple");
+    setColor(4, 3, "purple");
+    setColor(5, 4, "purple");
+     setColor(5, 5, "purple");
+      setColor(6, 6, "purple");
+       setColor(7, 5, "purple");
+
